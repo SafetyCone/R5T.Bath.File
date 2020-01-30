8ba423776cabec13f797a4675df8dbdd25a66e09
MinexAutomation <minex.automation@gmail.com>
2020-01-29 23:22:41 -0800

    Added ServiceAction<T>-related IServiceCollection extensions.

---
+++ source/R5T.Bath.File/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Bath.File
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFileHumanOutput(this IServiceCollection services,
+            ServiceAction<IHumanOutputFilePathProvider> addHumanOutputFilePathProvider)
+        {
+            services
+                .AddSingleton<IHumanOutput, FileHumanOutput>()
+                .RunServiceAction(addHumanOutputFilePathProvider)
+                ;
+
+            return services;
+        }
+
+        public static ServiceAction<IHumanOutput> AddFileHumanOutputAction(this IServiceCollection services,
+            ServiceAction<IHumanOutputFilePathProvider> addHumanOutputFilePathProvider)
+        {
+            var serviceAction = new ServiceAction<IHumanOutput>(() => services.AddFileHumanOutput(addHumanOutputFilePathProvider));
+            return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="SpecifiedHumanOutputFilePathProvider"/> implementation of <see cref="IHumanOutputFilePathProvider"/>.
+        /// </summary>
+        public static IServiceCollection AddSpecifiedHumanOutputFilePathProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<IHumanOutputFilePathProvider, SpecifiedHumanOutputFilePathProvider>();
+
+            return services;
+        }
+
+        public static ServiceAction<IHumanOutputFilePathProvider> AddSpecifiedHumanOutputFilePathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IHumanOutputFilePathProvider>(() => services.AddSpecifiedHumanOutputFilePathProvider());
+            return serviceAction;
+        }
+    }
+}
